46d1e1eae8f716aa57e9c2abc427dc8f43fbcf5b
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-03-26 14:09:40 +0000

    Update makePCKiosk.py

--- makePCKiosk.py
+++ makePCKiosk.py
@@ -9,7 +9,7 @@ configHandle = open("/home/pi/.config/autostart/kiosk.desktop", "w")
 configHandle.write("[Desktop Entry]\n")
 configHandle.write("Type=application\n")
 configHandle.write("Name=Kiosk\n")
-configHandle.write("Exec=/usr/bin/chromium --incognito --start-maximized --no-default-browser-check https://remote.knightsbridgeschool.com\n")
+configHandle.write("Exec=/usr/bin/chromium --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
 configHandle.close()
 
 os.system("sudo reboot")
