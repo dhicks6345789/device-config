3d95213018cbdff5ff095b7e3995d094acfebd7f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-03-26 14:32:21 +0000

    Update makePCKiosk.py

--- makePCKiosk.py
+++ makePCKiosk.py
@@ -4,12 +4,14 @@
 import os
 
 print("Configuring...")
-os.makedirs("/home/pi/.config/autostart", exist_ok=True)
-configHandle = open("/home/pi/.config/autostart/kiosk.desktop", "w")
-configHandle.write("[Desktop Entry]\n")
-configHandle.write("Type=application\n")
-configHandle.write("Name=Kiosk\n")
-configHandle.write("Exec=/usr/bin/chromium --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
-configHandle.close()
+print("/etc/xdg/lxsession/LXDE-pi/autostart")
 
-os.system("sudo reboot")
+#os.makedirs("/home/pi/.config/autostart", exist_ok=True)
+#configHandle = open("/home/pi/.config/autostart/kiosk.desktop", "w")
+#configHandle.write("[Desktop Entry]\n")
+#configHandle.write("Type=application\n")
+#configHandle.write("Name=Kiosk\n")
+#configHandle.write("Exec=/usr/bin/chromium --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
+#configHandle.close()
+
+#os.system("sudo reboot")
