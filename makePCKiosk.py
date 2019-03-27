130fbe29ef610762d0824505607fc2482e4187ed
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-03-27 10:46:18 +0000

    Update makePCKiosk.py

--- makePCKiosk.py
+++ makePCKiosk.py
@@ -25,12 +25,4 @@ configHandle = open("/boot/grub/grub.conf","w")
 configHandle.write(configString)
 configHandle.close()
 
-#os.makedirs("/home/pi/.config/autostart", exist_ok=True)
-#configHandle = open("/home/pi/.config/autostart/kiosk.desktop", "w")
-#configHandle.write("[Desktop Entry]\n")
-#configHandle.write("Type=application\n")
-#configHandle.write("Name=Kiosk\n")
-#configHandle.write("Exec=/usr/bin/chromium --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
-#configHandle.close()
-
-#os.system("reboot")
+os.system("reboot")
