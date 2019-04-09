3efe8f8d008cac6305f0d8cd0f06a959048c7d86
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-04-09 18:18:43 +0100

    Update makePCKiosk.py

--- makePCKiosk.py
+++ makePCKiosk.py
@@ -2,8 +2,17 @@
 
 # Standard libraries.
 import os
+import sys
 
 print("Configuring...")
+
+if os.path.exists("/usr/bin/chromium"):
+  chromiumPath = "/usr/bin/chromium"
+elif os.path.exists("/usr/bin/chromium-browser"):
+  chromiumPath = "/usr/bin/chromium-browser"
+else:
+  print("Error - Chromium not installed.")
+  sys.exit(1)
 
 configHandle = open("/etc/xdg/lxsession/LXDE-pi/autostart","w")
 # To do: check these settings work okay.
@@ -11,18 +20,19 @@ configHandle.write("xset s noblank\n")
 configHandle.write("xset s off\n")
 configHandle.write("xset -dpms\n")
 configHandle.write("point-rpi\n")
-configHandle.write("/usr/bin/chromium --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
+configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
 configHandle.close()
 
-configString = ""
-configHandle = open("/boot/grub/grub.cfg")
-for configLine in configHandle.readlines():
-  configLine.replace("timeout=5","timeout=0")
-  configString = configString + configLine
-configHandle.close()
+if os.path.exists("/boot/grub/grub.cfg"):
+  configString = ""
+  configHandle = open("/boot/grub/grub.cfg")
+  for configLine in configHandle.readlines():
+    configLine.replace("timeout=5","timeout=0")
+    configString = configString + configLine
+  configHandle.close()
 
-configHandle = open("/boot/grub/grub.conf","w")
-configHandle.write(configString)
-configHandle.close()
+  configHandle = open("/boot/grub/grub.conf","w")
+  configHandle.write(configString)
+  configHandle.close()
 
-os.system("reboot")
+#os.system("reboot")
