2e4c65dd9f4eebe56951aa553ad9d66d15850043
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-04-24 15:46:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -36,15 +36,18 @@ def displayMenu(theMenu):
 #  print "Configuring system with the GOV.UK Jekyll environment..."  
 
 #sys.exit(1)
-  
+
 print("Re-writing GUI Autostart file.")
 configHandle = open("/etc/xdg/lxsession/LXDE-pi/autostart","w")
 configHandle.write("xset s noblank\n")
 configHandle.write("xset s off\n")
 configHandle.write("xset -dpms\n")
 configHandle.write("point-rpi\n")
-configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
+#configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
+configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check https://docs.google.com/presentation/d/e/2PACX-1vRstVVaPRpKUAgmU-IIwk4ywY_pzhqynhMqG7BJY8ya4tf_82G01RZL1TqVcLVCBI2xkfYL-oLLUyxB/pub?start=false&loop=false&delayms=3000\n")
 configHandle.close()
+
+sys.exit(1)
 
 if os.path.exists("/boot/grub/grub.cfg"):
   print("Removing boot timeout from grub.conf.")
