f1c9e6cb7f44ed016021d0313d3d99282caed74d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-09-17 14:13:56 +0100

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,30 @@ def displayMenu(theMenu):
     return(displayMenu(selectedOption))
   return(selectedOption)
 
+def setAutostart():
+  print("Re-writing GUI Autostart file.")
+  configHandle = open("/etc/xdg/lxsession/LXDE-pi/autostart","w")
+  configHandle.write("xset s noblank\n")
+  configHandle.write("xset s off\n")
+  configHandle.write("xset -dpms\n")
+  configHandle.write("point-rpi\n")
+  configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
+  #configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check --start-fullscreen https://docs.google.com/presentation/d/e/2PACX-1vRstVVaPRpKUAgmU-IIwk4ywY_pzhqynhMqG7BJY8ya4tf_82G01RZL1TqVcLVCBI2xkfYL-oLLUyxB/pub?start=true&loop=true&delayms=6000\n")
+  configHandle.close()
+
+def removeGrubBootTimeout():
+  if os.path.exists("/boot/grub/grub.cfg"):
+    print("Removing boot timeout from grub.conf.")
+    configString = ""
+    configHandle = open("/boot/grub/grub.cfg")
+    for configLine in configHandle.readlines():
+      configString = configString + configLine.replace("timeout=5","timeout=0")
+    configHandle.close()
+    
+    configHandle = open("/boot/grub/grub.conf","w")
+    configHandle.write(configString)
+    configHandle.close()
+    
 menuResult = displayMenu(menu)
 if menuResult == "pythonHugo":
   print("Configuring system with Python and Hugo...")
@@ -37,31 +61,3 @@ elif menuResult == "govukJekyll":
   print("Configuring system with the GOV.UK Jekyll environment...")
 elif menuResult == "dataloggingKiosk":
   print("Configuring system as a Science Datalogging Kiosk...")
-
-sys.exit(1)
-
-print("Re-writing GUI Autostart file.")
-configHandle = open("/etc/xdg/lxsession/LXDE-pi/autostart","w")
-configHandle.write("xset s noblank\n")
-configHandle.write("xset s off\n")
-configHandle.write("xset -dpms\n")
-configHandle.write("point-rpi\n")
-configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk https://remote.knightsbridgeschool.com\n")
-#configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check --start-fullscreen https://docs.google.com/presentation/d/e/2PACX-1vRstVVaPRpKUAgmU-IIwk4ywY_pzhqynhMqG7BJY8ya4tf_82G01RZL1TqVcLVCBI2xkfYL-oLLUyxB/pub?start=true&loop=true&delayms=6000\n")
-configHandle.close()
-
-#sys.exit(1)
-
-if os.path.exists("/boot/grub/grub.cfg"):
-  print("Removing boot timeout from grub.conf.")
-  configString = ""
-  configHandle = open("/boot/grub/grub.cfg")
-  for configLine in configHandle.readlines():
-    configString = configString + configLine.replace("timeout=5","timeout=0")
-  configHandle.close()
-
-  configHandle = open("/boot/grub/grub.conf","w")
-  configHandle.write(configString)
-  configHandle.close()
-
-os.system("reboot")
