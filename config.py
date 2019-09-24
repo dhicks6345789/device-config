6a66f3cbf2438b9dff7ab5d1e8de885dad42fe24
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-09-24 12:22:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -98,7 +98,7 @@ elif menuResult == "dataloggingKiosk":
   expectFile.write("\n")
   expectFile.close()
   os.syste("expect rclone.expect")
-  os.system("rm rclone.expect")
+  #os.system("rm rclone.expect")
   print("Set boot process to hand over to web-editable script (owned by the datalogging user) to run logging software, Chrome, or anything else needed.")
   autorunFile = open("/home/pi/autorun.sh", "w")
   autorunFile.write("sleep 10\n")
