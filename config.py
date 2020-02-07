c0d81024c6dfda14cae8c9085163e27aa9d319c4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-02-07 11:43:24 +0000

    Update config.py

--- config.py
+++ config.py
@@ -265,7 +265,7 @@ elif menuResult == "webBrowsingMachine":
       output.append(rcDataLine)
     if rcDataLine.strip() == "</applications>":
       inApplications = False
-  writeFileFromArray("temp.xml", output)
+  writeFileFromArray("/etc/xdg/openbox/lxde-pi-rc.xml", output)
   
   writeFileFromArray("/home/pi/autorun.sh", [
     "sleep 4",
