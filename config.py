6d270faf43b8436cf437ab385065ddef1528b9de
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-02-20 13:38:46 +0000

    Update config.py

--- config.py
+++ config.py
@@ -271,7 +271,7 @@ elif menuResult == "webBrowsingMachine":
     "sleep 4",
     "amixer cset numid=3 1",
     "/usr/bin/python3 /home/pi/restartServer.py &",
-    chromiumPath + " --incognito --start-maximized --no-default-browser-check --disable-popup-blocking --disable-component-update --user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\" " + URL + " > /dev/null 2>&1",
+    chromiumPath + " --incognito --start-maximized --no-default-browser-check --disable-popup-blocking --disable-component-update --simulate-outdated-no-au=\"Tue, 31 Dec 2099 23:59:59 GMT\" --user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\" " + URL + " > /dev/null 2>&1",
     restartOrShutdown
   ])
   
