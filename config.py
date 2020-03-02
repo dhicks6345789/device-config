90f7f70f547a7632be4d30bf1a7f9a87fc22415c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-03-02 12:14:49 +0000

    Update config.py

--- config.py
+++ config.py
@@ -185,8 +185,32 @@ if menuResult == "pythonHugo":
 elif menuResult == "govukJekyll":
   print("Configuring system with the GOV.UK Jekyll environment...")
 elif menuResult == "webKisok":
-  print("Configuring system as a Web Kiosk...")
-  #configHandle.write(chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk https://something.example.com\n")
+  print("Configuring system as a Web Kiosk...")  
+  setHostname()
+  removeGrubBootTimeout()
+  print("On startup, load which URL?")
+  URL = getSetting("URL")
+  print("On browser exit, shutdown (s) or restart (r)?")
+  restartOrShutdown = getSetting("restartOrShutdown")
+  if restartOrShutdown == "s":
+    restartOrShutdown = "shutdown now"
+  else:
+    restartOrShutdown = "reboot"
+    
+  writeFileFromArray("/home/pi/autorun.sh", [
+    "sleep 4",
+    "amixer cset numid=3 1",
+    "/usr/bin/python3 /home/pi/restartServer.py &",
+    chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk --disable-popup-blocking --disable-component-update --simulate-outdated-no-au=\"Tue, 31 Dec 2099 23:59:59 GMT\" --user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\" " + URL + " > /dev/null 2>&1",
+    restartOrShutdown
+  ])
+  
+  writeFileFromArray("/var/spool/cron/crontabs/root", [
+    "15 03 * * * /sbin/reboot\n"
+  ])
+  os.system("chmod 0600 /var/spool/cron/crontabs/root")
+      
+  setAutostart(["@xset s off","@xset -dpms","@xset s noblank","bash /home/pi/autorun.sh"])
 elif menuResult == "dataloggingMachine":
   print("Configuring system as a Datalogging Machine...")
   setHostname()
