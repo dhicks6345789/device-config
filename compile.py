6dcbbf8d6fd2b2e0994df192cf2002503ec606f6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-03-01 13:38:05 +0000

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,9 +1,9 @@
 import os
 
-import device-config
+import deviceConfig
 
 print("STATUS: Compiling " + sys.argv[1] + " to " + sys.argv[2])
 
-libText = device-config.readFile("device-config.py")
-outputText = device-config.readFile(sys.argv[1])
-device-config.writeFile(sys.argv[2], outputText.replace("import device-config", libText))
+libText = deviceConfig.readFile("deviceConfig.py")
+outputText = deviceConfig.readFile(sys.argv[1])
+device-config.writeFile(sys.argv[2], outputText.replace("import deviceConfig", libText))
