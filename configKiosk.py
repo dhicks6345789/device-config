7655e08d79645a6fd19c0f1412154232d7e5f0b9
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-03-01 17:34:28 +0000

    Update configKiosk.py

--- configKiosk.py
+++ configKiosk.py
@@ -23,20 +23,20 @@ if restartOrShutdown == "s":
 else:
     restartOrShutdown = "reboot"
     
-#setHostname()
-#removeGrubBootTimeout()
+setHostname()
+removeGrubBootTimeout()
 
-#writeFile("/home/pi/autorun.sh", [
-#    "sleep 4",
-#    "amixer cset numid=3 1",
-#    "/usr/bin/python3 /home/pi/restartServer.py &",
-#    chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk --disable-popup-blocking --disable-component-update --simulate-outdated-no-au=\"Tue, 31 Dec 2099 23:59:59 GMT\" --user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\" " + URL + " > /dev/null 2>&1",
-#    restartOrShutdown
-#])
+writeFile("/home/pi/autorun.sh", [
+    "sleep 4",
+    "amixer cset numid=3 1",
+    chromiumPath + " --incognito --start-maximized --no-default-browser-check --kiosk --disable-popup-blocking --disable-component-update --simulate-outdated-no-au=\"Tue, 31 Dec 2099 23:59:59 GMT\" --user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\" " + URL + " > /dev/null 2>&1",
+    restartOrShutdown
+])
 
-#writeFile("/var/spool/cron/crontabs/root", [
-#    "15 03 * * * /sbin/reboot\n"
-#])
-#os.system("chmod 0600 /var/spool/cron/crontabs/root")
+writeFile("/var/spool/cron/crontabs/root", [
+    "15 03 * * * /sbin/reboot\n"
+])
+os.system("chmod 0600 /var/spool/cron/crontabs/root")
 
-#setAutostart(["@xset s off","@xset -dpms","@xset s noblank","bash /home/pi/autorun.sh"])
+#setPiAutostart(["@xset s off","@xset -dpms","@xset s noblank","bash /home/pi/autorun.sh"])
+setPiAutostart(["bash /home/pi/autorun.sh"])
