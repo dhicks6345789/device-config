e22416261e710bd13cfd738de964c039e2fba207
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-04-20 13:25:41 +0100

    Update compile.py

--- compile.py
+++ compile.py
@@ -4,10 +4,9 @@ import sys
 import deviceConfig
 
 for item in os.listdir():
-  if item not in ["compile.py"]:
+  if item not in ["compile.py", "README.md", "config.py", "__pycache__", ".git", "deviceConfig.py", "LICENSE"]:
     outputFile = sys.argv[1] + "/" + item
-    print("Compiling " + item + " to " + outputFile)
-    
-    #libText = deviceConfig.readFile("deviceConfig.py")
-    #outputText = deviceConfig.readFile(item)
-    #deviceConfig.writeFile(outputFile, outputText.replace("import deviceConfig", libText))
+    print("DeviceConfig - compiling " + item + " to " + outputFile)
+    libText = deviceConfig.readFile("deviceConfig.py")
+    outputText = deviceConfig.readFile(item)
+    deviceConfig.writeFile(outputFile, outputText.replace("import deviceConfig", libText))
